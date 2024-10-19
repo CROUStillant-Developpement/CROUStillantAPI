8acc6d84543f4d020fbb238f23a8608f8a6dbeed
PaulBayfield <137620008+PaulBayfield@users.noreply.github.com>
2024-10-19 23:45:09 +0200

    feat: Improved main file

--- __main__.py
+++ __main__.py
@@ -1,20 +1,9 @@
-import asyncio
-import uvloop
-
 from CROUStillantAPI.app import app
-from hypercorn.asyncio import serve
-from hypercorn.config import Config
 from dotenv import load_dotenv
 from os import environ
 
 
 load_dotenv(dotenv_path=f".env")
 
-
-config = Config()
-config.bind = [f"{environ.get('API_HOST', '0.0.0.0')}:{environ.get('API_PORT', 5000)}"]
-config.workers = 1
-
-
-uvloop.install()
-asyncio.run(serve(app, config))
+if __name__ == '__main__':
+    app.run(host=environ.get('API_HOST', '0.0.0.0'), port=int(environ.get('API_PORT', 5000)))
