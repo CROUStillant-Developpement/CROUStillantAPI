15d7b08d244cdd4cba08f13795076e50ba41dc54
PaulBayfield <137620008+PaulBayfield@users.noreply.github.com>
2024-10-20 16:45:37 +0200

    feat: Added custom Ratelimited Exception

---
+++ CROUStillantAPI/exceptions/ratelimit.py
@@ -0,0 +1,11 @@
+from typing import Any, Dict
+from sanic.exceptions import SanicException
+
+
+class RatelimitException(SanicException):
+    status_code = 429
+    
+    @property
+    def message(self):
+        return f"Vous avez envoyé trop de requêtes. Veuillez réessayer dans {self.extra['cooldown']} secondes."
+
