a10d0309fc114d75a46c9769b52f3b4af202341f
PaulBayfield <137620008+PaulBayfield@users.noreply.github.com>
2024-10-29 23:22:34 +0100

    fix: Fixed app debug with env

--- __main__.py
+++ __main__.py
@@ -7,4 +7,4 @@ load_dotenv(dotenv_path=f".env")
 
 
 if __name__ == '__main__':
-    app.run(host=environ.get('API_HOST', '0.0.0.0'), port=int(environ.get('API_PORT', 5000)), debug=environ.get("API_DEBUG", False))
+    app.run(host=environ.get('API_HOST', '0.0.0.0'), port=int(environ.get('API_PORT', 5000)), debug=True if environ.get("API_DEBUG") == "True" else False)
