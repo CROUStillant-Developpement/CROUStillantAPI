76930fec4ffb804c1775b24efb08593be0fe3cda
PaulBayfield <137620008+PaulBayfield@users.noreply.github.com>
2024-12-06 11:51:26 +0100

    feat: Added new components: Tache & TacheWithRestaurants

--- CROUStillantAPI/models/components/__init__.py
+++ CROUStillantAPI/models/components/__init__.py
@@ -3,3 +3,4 @@ from .restaurant import Restaurant as RestaurantComponent
 from .type_restaurant import TypeRestaurant as TypeRestaurantComponent
 from .plat import Plat as PlatComponent, PlatWithTotal as PlatComponentWithTotal
 from .menu import Menu as MenuComponent, Date as DateComponent
+from .tache import Tache as TacheComponent, TacheWithRestaurants as TacheWithRestaurantsComponent
