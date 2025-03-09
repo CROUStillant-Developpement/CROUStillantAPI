e65235a30c2a920f1717cd31dbc1f782e11e6522
PaulBayfield <137620008+PaulBayfield@users.noreply.github.com>
2025-03-09 14:44:00 +0100

    feat: New error handler

---
+++ CROUStillantAPI/components/errors.py
@@ -0,0 +1,38 @@
+from .ratelimit import ratelimit
+from .response import JSON
+from sanic.exceptions import NotFound, SanicException
+
+
+class ErrorHandler:
+    """
+    Classe pour gérer les erreurs
+    """
+    def __init__(self, app):
+        """
+        Constructeur de la classe
+        """
+        self.app = app
+
+
+        @app.exception(NotFound)
+        @ratelimit()
+        async def not_found(request, exception):
+            return JSON(
+                request=request,
+                success=False,
+                message="La ressource demandée n'existe pas.",
+                status=404
+            ).generate()
+
+
+        @app.exception(Exception, SanicException)
+        @ratelimit()
+        async def handle_exception(request, exception):
+            self.app.ctx.logs.error(f"Erreur: {exception}")
+
+            return JSON(
+                request=request,
+                success=False,
+                message=exception.message if hasattr(exception, "message") else "Une erreur s'est produite lors du traitement de votre requête. Nous nous excusons pour la gêne occasionnée, notre équipe est sur le coup !",
+                status=500
+            ).generate()
