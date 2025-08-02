b5467b650d5448daf6557149fa5720d5c87b1aac
Paul Bayfield <137620008+PaulBayfield@users.noreply.github.com>
2025-08-02 21:31:19 +0200

    feat: Optimised stats loading

--- CROUStillantAPI/entities/stats.py
+++ CROUStillantAPI/entities/stats.py
@@ -15,35 +15,8 @@ class Stats:
         async with self.pool.acquire() as connection:
             connection: Connection
 
-            # return await connection.fetchrow(
-            #     """
-            #         SELECT
-            #             (SELECT COUNT(*) FROM REGION) AS regions,
-            #             (SELECT COUNT(*) FROM RESTAURANT) AS restaurants,
-            #             (SELECT COUNT(*) FROM TYPE_RESTAURANT) AS types_restaurants,
-            #             (SELECT COUNT(*) FROM MENU) AS menus,
-            #             (SELECT COUNT(*) FROM REPAS) AS repas,
-            #             (SELECT COUNT(*) FROM CATEGORIE) AS categories,
-            #             (SELECT COUNT(*) FROM PLAT) AS plats,
-            #             (SELECT COUNT(*) FROM COMPOSITION) AS compositions
-            #     """
-            # )
-
-            # Pour des raisons de performances, on récupère les statistiques depuis la dernière tâche au lieu de les calculer à chaque fois
             return await connection.fetchrow(
                 """
-                    SELECT
-                        FIN_REGIONS AS regions,
-                        FIN_RESTAURANTS AS restaurants,
-                        FIN_ACTIFS AS restaurants_actifs,
-                        FIN_TYPES_RESTAURANTS AS types_restaurants,
-                        FIN_MENUS AS menus,
-                        FIN_REPAS AS repas,
-                        FIN_CATEGORIES AS categories,
-                        FIN_PLATS AS plats,
-                        FIN_COMPOSITIONS AS compositions
-                    FROM TACHE
-                    ORDER BY ID DESC
-                    LIMIT 1
+                    SELECT * FROM v_stats;
                 """
             )
